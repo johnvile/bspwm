c3d4b7a9480da6a277c61261abe42a1c12695103
dsfadsfgafgf <johnvile@gmail.com>
2020-07-09 09:24:11 +0100

    Add files via upload

--- autostart.sh
+++ autostart.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 xrandr --output eDP --mode 1366x768 --pos 215x0 --rotate normal --output HDMI-0 --primary --mode 1920x1080 --pos 0x838 --rotate normal --output VGA-0 --off &
-dex -a ~/.config/autostart/ & # tint2, redshift, nm-aplet, volumeicon.
+dex -a ~/.config/autostart/ & # tint2, redshift, nm-aplet, volumeicon or pasystray (whatever works)
 unclutter &
 nitrogen --restore &
-#sleep 05 &
+sleep 05 &
 #xcompmgr &
-#pcmanfm -d &
+pcmanfm -d &
